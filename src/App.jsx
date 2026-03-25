dc05f81b70313beb679ace54c6a0aaf626ea7341
Anmol Chourasia <anmol.chourasia@developer.com>
2026-03-25 19:56:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import BabylonScene from "./BabylonScene";
 import PropertiesPanel from "./PropertiesPanel";
 import StatusBar from "./StatusBar";
-import Toolbar from "./Toolbar";
+import Toolbar from "./ToolBar";
 
 export default function App() {
   return (
